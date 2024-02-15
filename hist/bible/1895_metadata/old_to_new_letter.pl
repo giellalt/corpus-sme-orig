176346dc30ed953917ecc6f19d7bd9be1e85e09d
Trond Trosterud <trond.trosterud@uit.no>
2024-02-15 15:35:17 +0100

    caron and not macron

---
+++ hist/bible/1895_metadata/old_to_new_letter.pl
@@ -0,0 +1,18 @@
+﻿#!/usr/bin/perl -w
+
+use strict;
+use utf8;
+use feature 'unicode_strings';
+
+while(<>) {
+s/c̄/č/g;
+s/C̄/Č/g;
+s/ʒ̄/ǯ/g;
+s/s̄/š/g;
+s/S̄/Š/g;
+
+	my $line = $_;
+
+	print $line;
+}
+
